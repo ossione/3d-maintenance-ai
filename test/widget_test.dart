dd74fc77a08c97dc0392ab50cd5f480a52f4ac9d
ossione <ossione0611@gmail.com>
2026-08-31 14:45:18 -0300

    Create widget_test.dart

---
+++ test/widget_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:putiz_9/main.dart';
+
+void main() {
+  testWidgets('App starts and shows home page', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaintenanceApp());
+    expect(find.text('3D Maintenance AI'), findsOneWidget);
+  });
+}
