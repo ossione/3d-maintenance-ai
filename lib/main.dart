8095caf924c52be132d6bcfee18fde6b5250b875
ossione <ossione0611@gmail.com>
2026-08-31 14:02:24 -0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+dart
+import 'package:flutter/material.dart';
+import 'features/home_page.dart';
+
+void main() => runApp(const MaintenanceApp());
+
+class MaintenanceApp extends StatelessWidget {
+  const MaintenanceApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '3D Maintenance AI',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1565C0)),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
